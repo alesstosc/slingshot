0b428feea741daeb282085dbada78ce578ab63bf
Alex "SUD" Toscano <alesstosc@gmail.com>
2023-09-11 21:21:06 +0200

    Add files via upload

--- index_Media/Script.js
+++ index_Media/Script.js
@@ -1,4 +1,13 @@
-function Link1_OnClick(){GetUrl("http://www.google.com");
+// Utilizza Detect.js per rilevare il tipo di dispositivo
+var tipoDispositivo = detect.parse(navigator.userAgent);
+
+// Verifica il tipo di dispositivo
+if (tipoDispositivo.mobile) {
+console.log("Questo dispositivo è un cellulare.");
+} else if (tipoDispositivo.tablet) {
+console.log("Questo dispositivo è un tablet.");
+} else {
+GetUrl("http://www.google.com");
 
 }
 
