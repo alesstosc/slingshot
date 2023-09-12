f2663a66f7e64c1b79a594ccbcc3e452eaf22ae0
Alex "SUD" Toscano <alesstosc@gmail.com>
2023-09-12 13:59:20 +0200

    Add files via upload

--- index_Media/Script.js
+++ index_Media/Script.js
@@ -1,4 +1,25 @@
 function Frame__0_0(ID){alert("UserAgent: " + navigator.userAgent + "\nappName: " + navigator.appName + "\nappVersion: " + navigator.appVersion);
+// Ottieni la stringa userAgent
+var userAgentString = navigator.userAgent;
+
+// Dividi la stringa in parole utilizzando uno o più spazi come delimitatori
+var parole = userAgentString.split(/\s+/);
+
+// Utilizza un ciclo for per enumerare le parole
+var contatore=0;
+for (var i = 0; i < parole.length; i++){
+	if (parole[i]=="(Windows") {
+		contatore=contatore+1;
+	}
+}
+if (contatore==1){
+	GetUrl("http://www.microsoft.com");
+} else{
+	GetUrl("http://www.google.com");
+}
+	
+
+
 
 }
 
